ded329e2ff2ce38a964d059ec542b084e40a1c41
casept <casept@users.noreply.github.com>
2016-11-23 19:51:01 +0100

    Build syncthing from source

--- armhf/build/syncthing/install.sh
+++ armhf/build/syncthing/install.sh
@@ -1,48 +1,21 @@
 #!/bin/sh
-BUILDDEPS="openssl gnupg"
+BUILDDEPS="libressl gnupg git go musl-dev"
 RUNDEPS="ca-certificates dumb-init"
 
 apk add -U $BUILDDEPS $RUNDEPS
 
-#TODO: automatically bump versions or use official alpine packages
-mkdir -p /tmp/syncthing && cd /tmp/syncthing
+mkdir -p /gopath/src/github.com/syncthing
+cd /gopath/src/github.com/syncthing
 
 #Download
-wget "https://github.com/syncthing/syncthing/releases/download/v"$ST_VERSION"/syncthing-linux-"$ST_ARCH"-v"$ST_VERSION".tar.gz"
-wget "https://github.com/syncthing/syncthing/releases/download/v"$ST_VERSION"/sha256sum.txt.asc"
+wget "https://github.com/syncthing/syncthing/archive/v"$ST_VERSION".tar.gz"
 
-
-#Download key and verify PGP signature
-#gpg --keyserver pool.sks-keyservers.net --recv-key D26E6ED000654A3E
-
-#We first have to verify the sha256 checksum file, then use that to estabilish whether the tarball is authentic
-#ISGOODSIG=`gpg --verify sha256sum.txt.asc | grep "Good signature from"`
-
-
-
-#echo $ISGOODSIG
-
-
-#if [ "$ISGOODSIG" = 'gpg: Good signature from "Syncthing Release Management <release@syncthing.net>" [unknown]' ] ; then
-#	echo "Checksum file signature is valid, proceeding."
-#else
-#	echo "Invalid signature for checksum file! Either you're under attack or the keyserver is down. Aborting."
-#	exit 1
-#fi
-#
-#
-#if sha256sum -c sha256sum.txt.asc | grep "OK" = "syncthing-linux-"$ARCH"-v"$VERSION".tar.gz: OK" ; then
-#	echo "Hash matches, tarball is authentic."
-#else
-#	echo "Invalid tarball checksum! Aborting."
-#	exit 1
-#fi
-#
-#}
-
-#Extract tarball and move binary
-tar -zxvf "syncthing-linux-"$ST_ARCH"-v"$ST_VERSION".tar.gz"
-mv syncthing*/syncthing /usr/local/bin/
+#Extract tarball and compile
+tar -zxvf "v"$ST_VERSION".tar.gz"
+mv "syncthing-"$ST_VERSION syncthing
+cd syncthing
+go run build.go -no-upgrade
+cp bin/syncthing /usr/local/bin/syncthing
 
 #Make sure binary is owned by root
 chown root:root /usr/local/bin/syncthing
@@ -57,9 +30,6 @@ chown -R syncthing /home/syncthing/.config/syncthing
 mkdir -p /home/syncthing/files
 chown -R syncthing /home/syncthing/files
 
-
-
 #Cleanup
 apk del $BUILDDEPS && rm -rf /var/cache/apk*
-rm -rf /root/.gnupg
-rm -rf /tmp/syncthing/*
+rm -rf /gopath
