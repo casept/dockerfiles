0b8900a447270dd59bc528fc3c4fc581cb37ca00
casept <casept@users.noreply.github.com>
2016-12-04 08:47:33 +0100

    Add rm=true to work around a circleci bug

--- ci/build_base.sh
+++ ci/build_base.sh
@@ -4,12 +4,12 @@ set -e
 build_amd64_base()  {
 cd amd64/build
 	cd debian-autobuild
-	docker build -t $DOCKER_USER"/"debian-autobuild:amd64 .
+	docker build --rm=false -t $DOCKER_USER"/"debian-autobuild:amd64 .
 	docker run -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ~/.docker/:/root/.docker/ --privileged -t $DOCKER_USER"/"debian-autobuild:amd64
 	cd ..
 
 	cd alpine-autobuild
-	docker build -t $DOCKER_USER"/"alpine-autobuild:amd64 .
+	docker build --rm=false -t $DOCKER_USER"/"alpine-autobuild:amd64 .
 	docker run -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ~/.docker/:/root/.docker/ -t $DOCKER_USER"/"alpine-autobuild:amd64
 	cd ../../..
 }
