725623a70a7c82d4137bf7ac81bcd326e7332fc4
casept <casept@users.noreply.github.com>
2016-12-04 08:43:09 +0100

    Remove dependency on docker-compose in ci/build_base.sh

--- ci/build_base.sh
+++ ci/build_base.sh
@@ -2,15 +2,16 @@
 set -e
 
 build_amd64_base()  {
-cd amd64/compose
+cd amd64/build
 	cd debian-autobuild
-	docker-compose build && docker-compose up 
+	docker build -t $DOCKER_USER"/"debian-autobuild:amd64 .
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ~/.docker/:/root/.docker/ --privileged -t $DOCKER_USER"/"debian-autobuild:amd64
 	cd ..
 
 	cd alpine-autobuild
-	docker-compose build && docker-compose up
+	docker build -t $DOCKER_USER"/"alpine-autobuild:amd64 .
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ~/.docker/:/root/.docker/ -t $DOCKER_USER"/"alpine-autobuild:amd64
 	cd ../../..
 }
 
 build_amd64_base
-
