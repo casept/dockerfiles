d67501636bf022d4e583b45f3f8c238f9b67cfb5
casept <casept@users.noreply.github.com>
2016-12-15 06:56:20 +0100

    Change graphdriver in vagrant box to overlay2

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,6 +7,15 @@ echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc
 #Install docker
 sudo apt update && sudo apt install -y --force-yes docker-engine
 
+#Change graphdriver to overlayfs2
+sudo service docker stop
+#Remove the docker directory because it's already polluted by devicemapper
+sudo rm -rf /var/lib/docker
+#Actually set the graphdriver
+sudo sed -i "s#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd --storage-driver=overlay2 -H fd://#g" /lib/systemd/system/docker.service
+sudo systemctl daemon-reload
+sudo service docker start
+
 #Install docker-compose
 sudo apt install -y --force-yes python3-pip
 sudo pip3 install docker-compose
