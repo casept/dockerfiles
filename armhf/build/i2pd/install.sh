805839da2f3a70bf8a16ce6e7330a8f80c5b10b0
casept <casept@users.noreply.github.com>
2017-02-04 16:48:03 +0100

    Base i2pd on alpine instead of debian

--- armhf/build/i2pd/install.sh
+++ armhf/build/i2pd/install.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
-RUNDEPS="dumb-init zlib1g openssl libboost-chrono1.61.0 libboost-date-time1.61.0 libboost-filesystem1.61.0 libboost-program-options1.61.0 libboost-system1.61.0 libboost-thread1.61.0"
-BUILDDEPS="cmake build-essential git libboost1.61-all-dev libssl-dev"
+set -e
+RUNDEPS="dumb-init zlib libressl boost"
+BUILDDEPS="cmake make gcc musl-dev g++ git zlib-dev libressl-dev boost-dev"
 
 #install dependencies and download source
-apt update && apt install -y --force-yes $RUNDEPS $BUILDDEPS
+apk update && apk add $RUNDEPS $BUILDDEPS
 mkdir /tmp/build && cd /tmp/build
 git clone https://github.com/PurpleI2P/i2pd.git
 
 #build and install
 cd i2pd/build
 cmake -DCMAKE_BUILD_TYPE=Release .
-make
+make -j2
 make install
 
 #create i2pd user
-adduser --system --uid 897 --home /home/i2pd i2pd
+adduser -S -u 897 -D -s /bin/false -h /home/i2pd i2pd
 
 #perform cleanup
 rm -rf /tmp/build
-apt-get remove -y --purge $BUILDDEPS && apt-get autoremove -y --purge
-apt-get clean
+apk del $BUILDDEPS
+rm -rf /var/cache/apk/*
