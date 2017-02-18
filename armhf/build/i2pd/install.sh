3f1efc19daa78addd2c212db3b098a7b7c5b2a08
casept <davids.paskevics@gmail.com>
2017-02-18 17:04:09 +0100

    Switch to building released versions of i2pd

--- armhf/build/i2pd/install.sh
+++ armhf/build/i2pd/install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 RUNDEPS="dumb-init zlib libressl boost"
-BUILDDEPS="cmake make gcc musl-dev g++ git zlib-dev libressl-dev boost-dev"
+BUILDDEPS="cmake make gcc musl-dev g++ zlib-dev libressl-dev boost-dev ca-certificates"
 
 #install dependencies and download source
 apk update && apk add $RUNDEPS $BUILDDEPS
 mkdir /tmp/build && cd /tmp/build
-git clone https://github.com/PurpleI2P/i2pd.git
+wget https://github.com/PurpleI2P/i2pd/archive/2.12.0.tar.gz
+tar -zxvf $I2PD_VERSION".tar.gz"
 
 #build and install
-cd i2pd/build
+cd "i2pd-"$I2PD_VERSION"/build" 
 cmake -DCMAKE_BUILD_TYPE=Release .
 make -j2
 make install
